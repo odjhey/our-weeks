ee152a9cb10ff9e1b1bf9cfa7e1707e73b35f23a
odjhey <odjhey@gmail.com>
2023-10-12 19:09:38 +0800

    chore: install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import dayjs from 'dayjs'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const fromDate = dayjs()
+  const toDate = dayjs().add(7, 'years')
+
+  // array of weeks from date to date
+  const weeks = Array.from({ length: toDate.diff(fromDate, 'week') + 1 }, (_, i) => fromDate.add(i, 'week'))
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-row flex-wrap gap-1 justify-center items-center'>
+      {weeks.map((week, idx) => {
+        return <div key={week.toISOString()}
+          className='w-10 h-10 bg-primary m-auto'
+        >{idx + 1}</div>
+      })}
+    </div>
   )
 }
 
